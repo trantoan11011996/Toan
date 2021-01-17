ddf8c3eb43ba55bf871e127e3f96a2ff55eed602
Toan <git remote add origin https://github.com/trantoan11011996/Toan.git>
2021-01-17 21:26:39 +0700

    app emoji2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/header'
-import { Searchbar } from './components/searchbar';
-import { Emojilist } from './components/emojlist';
-import { Component, component } from 'react'
+import { Lightbulb } from './components/Lightbulb'
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      kw : ' '
-    } 
-    this.inputvalue = this.inputvalue.bind(this)
-  }
-  inputvalue(event){
-    this.setState({kw : event.target.value} )
-  }
-  render(){
-    return (
-      <div className='App'>
-        <Header />
-        <Searchbar kw = {this.state.kw} change={this.inputvalue} />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <Lightbulb/>
+  );
 }
+
 export default App;
