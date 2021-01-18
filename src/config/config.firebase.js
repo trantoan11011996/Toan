05751f578196b339ee9495b285293ae0dd20cc9c
Toan <git remote add origin https://github.com/trantoan11011996/Toan.git>
2021-01-18 19:49:00 +0700

    update gitignore

---
+++ src/config/config.firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACuiAl4LwiM81q5XHq38XlUVZNBnRbamE",
+    authDomain: "react-d06.firebaseapp.com",
+    projectId: "react-d06",
+    storageBucket: "react-d06.appspot.com",
+    messagingSenderId: "289116126639",
+    appId: "1:289116126639:web:f1043013ac228f9e72435a"
+  };
