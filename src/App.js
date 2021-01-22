9efa5732c0c0696cdb3916389b15ad9e9bce9227
Toan <git remote add origin https://github.com/trantoan11011996/Toan.git>
2021-01-22 19:38:09 +0700

    firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import './App.css';
-import {Showinfo} from './ultis/ultis.js'
+import {addTodo,edittodo} from './ultis/ultis.js'
+// signup({username : 'minhben1997', password : 'minh1234'})
+//   .then(data =>{
+//     console.log('data: ',data)
+//   })
+//   .catch(er =>{
+//     console.error(er.stack)
+//   })
+
+addTodo({
+  // todoID : '3ledK6vuQs69BHGIDzBb',
+  ownerid : '1',
+  title : 'morning working',
+  content : 'di cho, nau an, hoc lap trinh',
+})
+edittodo({
+  todoID : '3ledK6vuQs69BHGIDzBb',
+  ownerid : '1',
+  title : 'morning working vui qa la vui',
+  content : 'di cho, nau an di lam  vui qa la vui',
+}).then(data=>{
+  console.log(data)
+})
 function App() {
-  Showinfo()
   return(
     
     <nav className="navbar navbar-inverse">
--- src/ultis/ultis.js
+++ src/ultis/ultis.js
@@ -1,7 +1,72 @@
-import {firebaseConfig} from '../config/config.firebase'
+import { firebaseConfig } from '../config/config.firebase'
+import 'firebase/firestore'
+import firebase from 'firebase/app'
+try {
+    firebase.initializeApp(firebaseConfig)
+} catch (e) {
+    console.log('firebase app was installed')
+}
+    const db = firebase.firestore()
+// console.log('db : ', db)
+// function signup(userinfromation) {
+//     return new Promise((resolve, reject) => {
+//         const { username, password } = userinfromation
+//         db.collection('user').where('username', '==', username)
+//             .get()
+//             .then(querySnapshot => {
+//                 if (!querySnapshot.empty) {
+//                    throw new Error ('user was exsited')
+//                 }  
+//             })
+//             .then(()=>{
+//                 db.collection('user')
+//                 .add({
+//                     username:username,
+//                     password:password,
+//                 })
+//                 .then(data =>{
+//                     console.log('sign up succesful')
+//                     console.log('data id : ', data.id)
+//                     resolve(data.id)
+//                 })
+//             })
+//             .catch(err=>{
+//                 reject(err)
+//             })
 
-function Showinfo() {
-    console.log('firebase: ',firebaseConfig)
+//     })
+
+
+// }
+
+// export { signup }
+
+
+export function addTodo(todoinformation){
+    return new Promise((resolve,reject)=>{
+        const { title,ownerid,content }  = todoinformation
+         db.collection('todo').add({
+            title : title,
+            ownerid : ownerid,
+            content : content,
+        })
+        .then(data=>{
+            console.log(data.id)
+            console.log(title)
+            console.log(ownerid)
+            console.log(content)
+        })
+    })
 }
 
-export {Showinfo}
+export function edittodo(todoinformation){
+    return new Promise((resolve,reject)=>{
+        const { title,content,todoID }  = todoinformation
+         db.collection('todo').doc(todoID).set({
+            title : title,
+            // content : content,
+        })
+        console.log(title)
+        
+        })
+}
